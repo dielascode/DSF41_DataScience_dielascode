b5a765627d75ed32a5e5e202c4fa5f77a3954ab6
didil <faridfadilah70@gmail.com>
2025-07-23 15:50:21 +0700

    Intro Python

---
+++ intropython.py
@@ -0,0 +1,31 @@
+print('Halo dunia')
+
+teman_ds = "Haloooooo"
+print(teman_ds)
+
+x = 10
+x = 5
+y = "Asek"
+print(x)
+print(y)
+
+#get the type
+u = 3
+print(type(u))
+
+kalimat = '''
+Kita belajar python hari ini
+seru banget
+'''
+print(kalimat)
+
+kalimat2 = """
+Kita belajar python hari ini
+seru banget
+"""
+print(kalimat2)
+
+a = 20
+b = 10
+if b > a: 
+    print("b is greater than a")
